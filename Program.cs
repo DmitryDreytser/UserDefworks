b8b6aae45f0be8f9f8064fa5544003bdb24093bc
ddreidzer <maddad@mail.ru>
2014-06-03 02:21:27 +0400

    Теперь читает Userdef

--- Program.cs
+++ Program.cs
@@ -19,154 +19,219 @@ namespace UserDefworks
     {
         // The user needs to allocate an STATSTG array whose size is celt.
         [PreserveSig]
-        uint Next(uint celt,
-                    [MarshalAs(UnmanagedType.LPArray), Out]
-    	        System.Runtime.InteropServices.ComTypes.STATSTG rgelt,
-                    out uint pceltFetched);
+        uint Next(
+            uint celt,
+            [MarshalAs(UnmanagedType.LPArray), Out]
+			System.Runtime.InteropServices.ComTypes.STATSTG[] rgelt,
+            out uint pceltFetched
+        );
 
         void Skip(uint celt);
+
         void Reset();
 
         [return: MarshalAs(UnmanagedType.Interface)]
         IEnumSTATSTG Clone();
     }
-    public class StgStorage
-    {
 
-        [Flags]
-        public enum STGM : int
-        {
-            DIRECT = 0x00000000,
-            TRANSACTED = 0x00010000,
-            SIMPLE = 0x08000000,
-            READ = 0x00000000,
-            WRITE = 0x00000001,
-            READWRITE = 0x00000002,
-            SHARE_DENY_NONE = 0x00000040,
-            SHARE_DENY_READ = 0x00000030,
-            SHARE_DENY_WRITE = 0x00000020,
-            SHARE_EXCLUSIVE = 0x00000010,
-            PRIORITY = 0x00040000,
-            DELETEONRELEASE = 0x04000000,
-            NOSCRATCH = 0x00100000,
-            CREATE = 0x00001000,
-            CONVERT = 0x00020000,
-            FAILIFTHERE = 0x00000000,
-            NOSNAPSHOT = 0x00200000,
-            DIRECT_SWMR = 0x00400000,
-        }
-
-
-        [DllImport("ole32.dll")]
-        public static extern int StgIsStorageFile([MarshalAs(UnmanagedType.LPWStr)]
-    	                                        string pwcsName);
-
-        [DllImport("ole32.dll")]
-        public static extern int StgOpenStorage([MarshalAs(UnmanagedType.LPWStr)]
-    	                                string pwcsName,
-                                            IStorage pstgPriority,
-                                            STGM grfMode,
-                                            IntPtr snbExclude,
-                                            uint reserved,
-                                            out IStorage ppstgOpen);
-    }
-    
     [ComImport]
     [Guid("0000000b-0000-0000-C000-000000000046")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IStorage
+    interface IStorage
     {
-        void CreateStream(string pwcsName, uint grfMode, uint reserved1,
-                            uint reserved2, out IStream ppstm);
+        void CreateStream(
+            /* [string][in] */ string pwcsName,
+            /* [in] */ uint grfMode,
+            /* [in] */ uint reserved1,
+            /* [in] */ uint reserved2,
+            /* [out] */ out IStream ppstm);
 
-        void OpenStream(string pwcsName, IntPtr reserved1, uint grfMode,
-                        uint reserved2, out IStream ppstm);
+        void OpenStream(
+            /* [string][in] */ string pwcsName,
+            /* [unique][in] */ IntPtr reserved1,
+            /* [in] */ uint grfMode,
+            /* [in] */ uint reserved2,
+            /* [out] */ out IStream ppstm);
 
-        void CreateStorage(string pwcsName, uint grfMode, uint reserved1,
-                            uint reserved2, out IStorage ppstg);
+        void CreateStorage(
+            /* [string][in] */ string pwcsName,
+            /* [in] */ uint grfMode,
+            /* [in] */ uint reserved1,
+            /* [in] */ uint reserved2,
+            /* [out] */ out IStorage ppstg);
 
-        void OpenStorage(string pwcsName, IStorage pstgPriority, uint grfMode,
-                            IntPtr snbExclude, uint reserved, out IStorage ppstg);
+        void OpenStorage(
+            /* [string][unique][in] */ string pwcsName,
+            /* [unique][in] */ IStorage pstgPriority,
+            /* [in] */ uint grfMode,
+            /* [unique][in] */ IntPtr snbExclude,
+            /* [in] */ uint reserved,
+            /* [out] */ out IStorage ppstg);
 
-        void CopyTo(uint ciidExclude, Guid rgiidExclude, IntPtr snbExclude,
-                    IStorage pstgDest);
+        void CopyTo(
+            /* [in] */ uint ciidExclude,
+            /* [size_is][unique][in] */ Guid rgiidExclude, // should this be an array?
+            /* [unique][in] */ IntPtr snbExclude,
+            /* [unique][in] */ IStorage pstgDest);
 
-        void MoveElementTo(string pwcsName, IStorage pstgDest,
-                            string pwcsNewName, uint grfFlags);
+        void MoveElementTo(
+            /* [string][in] */ string pwcsName,
+            /* [unique][in] */ IStorage pstgDest,
+            /* [string][in] */ string pwcsNewName,
+            /* [in] */ uint grfFlags);
 
-        void Commit(uint grfCommitFlags);
+        void Commit(
+            /* [in] */ uint grfCommitFlags);
 
         void Revert();
 
-        void EnumElements(uint reserved1, IntPtr reserved2, uint reserved3,
-                            out IEnumSTATSTG ppenum);
+        void EnumElements(
+            /* [in] */ uint reserved1,
+            /* [size_is][unique][in] */ IntPtr reserved2,
+            /* [in] */ uint reserved3,
+            /* [out] */ out IEnumSTATSTG ppenum);
 
-        void DestroyElement(string pwcsName);
+        void DestroyElement(
+            /* [string][in] */ string pwcsName);
 
-        void RenameElement(string pwcsOldName, string pwcsNewName);
+        void RenameElement(
+            /* [string][in] */ string pwcsOldName,
+            /* [string][in] */ string pwcsNewName);
 
-        void SetElementTimes(string pwcsName,
-                            System.Runtime.InteropServices.ComTypes.FILETIME pctime,
-                                System.Runtime.InteropServices.ComTypes.FILETIME patime,
-                                System.Runtime.InteropServices.ComTypes.FILETIME pmtime);
+        void SetElementTimes(
+            /* [string][unique][in] */ string pwcsName,
+            /* [unique][in] */ System.Runtime.InteropServices.ComTypes.FILETIME pctime,
+            /* [unique][in] */ System.Runtime.InteropServices.ComTypes.FILETIME patime,
+            /* [unique][in] */ System.Runtime.InteropServices.ComTypes.FILETIME pmtime);
 
-        void SetClass(Guid clsid);
+        void SetClass(
+            /* [in] */ Guid clsid);
 
-        void SetStateBits(uint grfStateBits, uint grfMask);
+        void SetStateBits(
+            /* [in] */ uint grfStateBits,
+            /* [in] */ uint grfMask);
 
-        void Stat(out System.Runtime.InteropServices.ComTypes.STATSTG pstatstg,
-                    uint grfStatFlag);
+        void Stat(
+            /* [out] */ out System.Runtime.InteropServices.ComTypes.STATSTG pstatstg,
+            /* [in] */ uint grfStatFlag);
+
     }
 
-    public class Lock
+    [Flags]
+    public enum STGM : int
     {
-        public IStorage OpenStorage(string fileName)
-        {
-            if (StgStorage.StgIsStorageFile(fileName) != 0)
-            {
-                return null;
-            }
-
-            IStorage storage = null;
-
-            //
-            // StgOpenStorage() locks file 'fileName'
-            //
-            // Set flags like:
-            // [http://stackoverflow.com/questions/1086814/opening-ole-compound-documents-read-only-with-stgopenstorage]
-            //
-            int stgOpenStorage = StgStorage.StgOpenStorage(fileName, null,
-                                                StgStorage.STGM.READ |
-                                                StgStorage.STGM.SHARE_DENY_NONE |
-                                                StgStorage.STGM.TRANSACTED,
-                                                IntPtr.Zero, 0,
-                                                out storage);
-
-            //
-            // Try to rename file (for testing purposes only)
-            //
-            try
-            {
-                File.Move(fileName, fileName + @".renamed");
-            }
-            catch (Exception ex)
-            {		// exception: file alreay in use by another process
-                throw;
-            }
-
-            if (stgOpenStorage != 0)
-            {
-                return null;
-            }
-            else
-            {
-                return storage;
-            }
-        }
+        DIRECT = 0x00000000,
+        TRANSACTED = 0x00010000,
+        SIMPLE = 0x08000000,
+        READ = 0x00000000,
+        WRITE = 0x00000001,
+        READWRITE = 0x00000002,
+        SHARE_DENY_NONE = 0x00000040,
+        SHARE_DENY_READ = 0x00000030,
+        SHARE_DENY_WRITE = 0x00000020,
+        SHARE_EXCLUSIVE = 0x00000010,
+        PRIORITY = 0x00040000,
+        DELETEONRELEASE = 0x04000000,
+        NOSCRATCH = 0x00100000,
+        CREATE = 0x00001000,
+        CONVERT = 0x00020000,
+        FAILIFTHERE = 0x00000000,
+        NOSNAPSHOT = 0x00200000,
+        DIRECT_SWMR = 0x00400000,
     }
 
+    public enum STATFLAG : uint
+    {
+        STATFLAG_DEFAULT = 0,
+        STATFLAG_NONAME = 1,
+        STATFLAG_NOOPEN = 2
+    }
+
+    public enum STGTY : int
+    {
+        STGTY_STORAGE = 1,
+        STGTY_STREAM = 2,
+        STGTY_LOCKBYTES = 3,
+        STGTY_PROPERTY = 4
+    }
+
+    
     class Program
     {
+
+        [DllImport("ole32.dll")]
+        private static extern int StgIsStorageFile(
+            [MarshalAs(UnmanagedType.LPWStr)] string pwcsName);
+
+        [DllImport("ole32.dll")]
+        static extern int StgOpenStorage(
+            [MarshalAs(UnmanagedType.LPWStr)] string pwcsName,
+            IStorage pstgPriority,
+            STGM grfMode,
+            IntPtr snbExclude,
+            uint reserved,
+            out IStorage ppstgOpen);
+
+        enum UserParameters
+        {
+            n = 0,
+            DontCheckRights,
+            PasswordHash,
+            FullName,
+            UserCatalog,
+            UserInterface,
+            UserRights
+        }
+
+
+        //Возвращаяет позицию параметра указзаного номера в потоке 
+        // 1 - контролировать права
+        // 2 - хэш пароля
+        // 3 - Полное имя пользователя
+        // 4 - каталог
+        // 5 - интерфейс
+        // 6 - Набор прав
+
+        static int GetPos(byte[] data, int Param)
+        {
+            int StartPosition = 0;
+            int Count = 0;
+            while (Count != (int)Param)
+            {
+                if (StartPosition >= data.Length)
+                     return 0;
+                if (StartPosition < 8)
+                {
+                    StartPosition += 4;
+                    Count++;
+                }
+                else
+                {
+                    StartPosition = StartPosition + data[StartPosition] + 1;
+
+                    if (data[StartPosition] == 1)
+                           StartPosition += 4;
+                    
+                    Count++;
+                }
+            }
+            return StartPosition;
+        }
+
+        static string GetParam(byte[] data, UserParameters paramNuber)
+        {
+            string param = string.Empty;
+            int paramstart = GetPos(data, (int) paramNuber);
+            if (paramNuber == UserParameters.DontCheckRights)
+            {
+                param = (1-data[paramstart]).ToString();
+            }
+            else
+                param = Encoding.Default.GetString(data).Substring(paramstart + 1, data[paramstart]);
+            return param;
+        }
+
+
         static void PrintHelp()
         {
             Console.WriteLine("Usage: "+ Path.GetFileName(Application.ExecutablePath).ToLower()  +" [/f|-f:<1CDatabasePath>\\usrdef\\users.usr]\r\n Where:\r\n - <1CDatabasePath> : full path to 1C database catalog.");
@@ -273,44 +338,85 @@ namespace UserDefworks
 
             }
 
-            IStorage storage = null;
-            IStorage Page_storage = null;
-            IStorage Page1_storage = null;
 
-            int IsStorage = StgStorage.StgIsStorageFile(USRfileName);
-
-            if (IsStorage == 0)
+            if (StgIsStorageFile(USRfileName) == 0)
             {
-                int stgOpenStorage = StgStorage.StgOpenStorage(USRfileName, null,
-                                                    StgStorage.STGM.READ |
-                                                    StgStorage.STGM.SHARE_DENY_NONE |
-                                                    StgStorage.STGM.TRANSACTED,
-                                                    IntPtr.Zero, 0,
-                                                    out storage);
-                if (stgOpenStorage == 0)
+                IStorage storage = null;
+                if (StgOpenStorage(
+                    USRfileName,
+                    null,
+                    STGM.DIRECT | STGM.READ | STGM.SHARE_EXCLUSIVE,
+                    IntPtr.Zero,
+                    0,
+                    out storage) == 0)
                 {
+                    System.Runtime.InteropServices.ComTypes.STATSTG statstg;
+                    storage.Stat(out statstg, (uint)STATFLAG.STATFLAG_DEFAULT);
 
+                    IEnumSTATSTG pIEnumStatStg = null;
+                    storage.EnumElements(0, IntPtr.Zero, 0, out pIEnumStatStg);
 
-                    storage.OpenStorage("Container.Cotents", Page_storage, 0, IntPtr.Zero, 0, out Page1_storage);
-                    IStream stt;
-                    storage.OpenStream("Container.Cotents",IntPtr.Zero, 0, 0, out stt);
+                    System.Runtime.InteropServices.ComTypes.STATSTG[] regelt = { statstg };
+                    uint fetched = 0;
+                    uint res = pIEnumStatStg.Next(1, regelt, out fetched);
 
-                    byte[] buf;
-                    ulong read;
-                    string str = stt.Read(buf, 255, ref read);
-
-                    IEnumSTATSTG ppStgEnum = null;
-                    System.Runtime.InteropServices.ComTypes.STATSTG Data = null;
-                    storage.EnumElements(0, IntPtr.Zero, 0, out ppStgEnum);
-                    uint i=0;
-
-                    while (ppStgEnum.Next(1, Data, out i) != 0) 
+                    if (res == 0)
                     {
-                        Console.WriteLine("{0} = {1}", Data[0], Data[1]);
+
+                        IStream pIStream = null;
+                        storage.OpenStream("Container.Contents",
+                                           IntPtr.Zero,
+                                           (uint)(STGM.READ | STGM.SHARE_EXCLUSIVE),
+                                           0,
+                                           out pIStream);
+                        if (pIStream != null)
+                        {
+                            System.Runtime.InteropServices.ComTypes.STATSTG StreamInfo;
+                            pIStream.Stat(out StreamInfo, 0);
+                            
+                            byte[] data = new byte[StreamInfo.cbSize];
+                            pIStream.Read(data, (int)StreamInfo.cbSize - 1, IntPtr.Zero);
+                            string UserContainer = Encoding.Default.GetString(data);
+
+                            UserContainer = UserContainer.Replace("{\"Container.Contents\",{", "");
+                            UserContainer = UserContainer.Replace("}}", "");
+                            UserContainer = UserContainer.Replace("},{", ";");
+
+                            foreach (string UserItem in UserContainer.Split(';'))
+                            {
+                                string UserName = (string)UserItem.Split(',').GetValue(2);
+                                UserName = UserName.Replace("\"", "");
+
+                                string UserPage = (string)UserItem.Split(',').GetValue(1);
+                                UserPage = UserPage.Replace("\"", "");
+
+                                storage.OpenStream(UserPage,
+                                                   IntPtr.Zero,
+                                                   (uint)
+                                                   (STGM.READ | STGM.SHARE_EXCLUSIVE),
+                                                   0,
+                                                   out pIStream);
+                                if (pIStream != null)
+                                {
+                                    pIStream.Stat(out StreamInfo, 0);
+                                    data = new byte[(int)StreamInfo.cbSize + 1];
+                                    pIStream.Read(data, (int)StreamInfo.cbSize, IntPtr.Zero);
+                                    Console.WriteLine("-----------{0}-----------", UserName);
+                                    for (int i = 1; i <= (int)UserParameters.UserRights; i++)
+                                    { 
+                                        Console.WriteLine(" {0} = {1}",(UserParameters)i, GetParam(data, (UserParameters)i));
+                                    }
+                                    Console.WriteLine("-------------------------", UserName);
+                                }
+
+
+
+                            }
+
+                        }
+
                     }
                 }
-                else
-                    Console.WriteLine("\r\nError opening {0}", USRfileName);
             }
 
             Console.WriteLine("\r\nPress any key to exit...");
